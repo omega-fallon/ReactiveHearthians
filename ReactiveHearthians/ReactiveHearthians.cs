fc3bb9d0fc158fbd32391cb6b8436072f832264d
omega-fallon <grbacs1970@gmail.com>
2024-04-18 09:41:20 -0400

    bad mallow dialogue

--- ReactiveHearthians/ReactiveHearthians.cs
+++ ReactiveHearthians/ReactiveHearthians.cs
@@ -14,6 +14,7 @@
 // Characters react to you dying in front of them
 // Characters react to you standing on top of them
 // Porphy reacts to you standing on their pot
+// Fix ATP pairing readout if you haven't paired to the statue (and also fix the one statue)
 
 // DONE LIST
 // option to tell Riebeck about the Stranger
@@ -81,6 +82,24 @@ namespace ReactiveHearthians
 
         public DarkMatterVolume darkmattervolume_arkose;
 
+        public Campfire slatefire;
+        public Campfire riebeckfire;
+        public Campfire eskerfire;
+        public Campfire chertfire;
+        public Campfire feldsparfire;
+
+        public bool slatefire_roasting;
+        public bool riebeckfire_roasting;
+        public bool eskerfire_roasting;
+        public bool chertfire_roasting;
+        public bool feldsparfire_roasting;
+
+        public float slatefire_badmallow_lastate;
+        public float riebeckfire_badmallow_lastate;
+        public float eskerfire_badmallow_lastate;
+        public float chertfire_badmallow_lastate;
+        public float feldsparfire_badmallow_lastate;
+
         private void Start()
         {
             // Starting here, you'll have access to OWML's mod helper.
@@ -98,6 +117,12 @@ namespace ReactiveHearthians
                 volumes = Resources.FindObjectsOfTypeAll<CowerAnimTriggerVolume>().ToList();
 
                 // Campfires people are sat near
+                slatefire = GameObject.Find("TimberHearth_Body/Sector_TH/Sector_Village/Interactables_Village/LaunchTower/Effects_HEA_Campfire/Controller_Campfire").GetComponent<Campfire>();
+                riebeckfire = GameObject.Find("BrittleHollow_Body/Sector_BH/Sector_Crossroads/Interactables_Crossroads/VisibleFrom_BH/Prefab_HEA_Campfire/Controller_Campfire").GetComponent<Campfire>();
+                eskerfire = GameObject.Find("Moon_Body/Sector_THM/Interactables_THM/Effects_HEA_Campfire/Controller_Campfire").GetComponent<Campfire>();
+                chertfire = GameObject.Find("CaveTwin_Body/Sector_CaveTwin/Sector_NorthHemisphere/Sector_NorthSurface/Sector_Lakebed/Interactables_Lakebed/Lakebed_VisibleFrom_Far/Prefab_HEA_Campfire/Controller_Campfire").GetComponent<Campfire>();
+                feldsparfire = GameObject.Find("DB_PioneerDimension_Body/Sector_PioneerDimension/Interactables_PioneerDimension/Prefab_HEA_Campfire/Controller_Campfire").GetComponent<Campfire>();
+
                 hazardvolume_slatefire = GameObject.Find("TimberHearth_Body/Sector_TH/Sector_Village/Interactables_Village/LaunchTower/Effects_HEA_Campfire/HeatHazardVolume").GetComponent<HazardVolume>();
                 hazardvolume_riebeckfire = GameObject.Find("BrittleHollow_Body/Sector_BH/Sector_Crossroads/Interactables_Crossroads/VisibleFrom_BH/Prefab_HEA_Campfire/HeatHazardVolume").GetComponent<HazardVolume>();
                 hazardvolume_eskerfire = GameObject.Find("Moon_Body/Sector_THM/Interactables_THM/Effects_HEA_Campfire/HeatHazardVolume").GetComponent<HazardVolume>();
@@ -111,6 +136,11 @@ namespace ReactiveHearthians
             GlobalMessenger.AddListener("TriggerSupernova", MakeAllCower);
             GlobalMessenger<string, bool>.AddListener("DialogueConditionChanged", MakeMicaCower);
             GlobalMessenger.AddListener("EnterDreamWorld",DreamWorldBeen);
+            GlobalMessenger.AddListener("TriggerMemoryUplink", StatueLinked);
+            GlobalMessenger.AddListener("EnterTimeLoopCentral", AshTwinFix);
+            GlobalMessenger<Campfire>.AddListener("EnterRoastingMode", EnterRoastingMode);
+            GlobalMessenger<Campfire>.AddListener("ExitRoastingMode", ExitRoastingMode);
+            GlobalMessenger<float>.AddListener("EatMarshmallow", EatMarshmallow);
         }
 
         // Harmony patches
@@ -278,6 +308,88 @@ namespace ReactiveHearthians
             PlayerData.SetPersistentCondition("DREAMWORLD_EVER_BEEN", true);
         }
 
+        // Statue linkup variable
+        public void StatueLinked()
+        {
+            PlayerData.SetPersistentCondition("RH_NOMAI_STATUE_LINKED", true);
+        }
+
+        // Statue readout & glow fix
+        public void AshTwinFix()
+        {
+
+        }
+
+        // C-a-m-p-f-i-r-e f-u-n-c funcs
+        public void EnterRoastingMode(Campfire campfire)
+        {
+            if (campfire == slatefire)
+            {
+                slatefire_roasting = true;
+            }
+            else if (campfire == eskerfire)
+            {
+                eskerfire_roasting = true;
+            }
+            else if (campfire == riebeckfire)
+            {
+                riebeckfire_roasting = true;
+            }
+            else if (campfire == chertfire)
+            {
+                chertfire_roasting = true;
+            }
+            else if (campfire == feldsparfire)
+            {
+                feldsparfire_roasting = true;
+            }
+        }
+        public void ExitRoastingMode(Campfire campfire)
+        {
+            slatefire_roasting = false;
+            eskerfire_roasting = false;
+            riebeckfire_roasting = false;
+            chertfire_roasting = false;
+            feldsparfire_roasting = false;
+        }
+        public List<BadMarshmallowCan> badcans;
+        public void EatMarshmallow(float toastedFraction)
+        {
+            if (badcans == null)
+            {
+                badcans = Resources.FindObjectsOfTypeAll<BadMarshmallowCan>().ToList();
+            }
+
+            if (badcans.Any(can => can._pickedUp) && toastedFraction < 1f)
+            {
+                if (slatefire_roasting)
+                {
+                    DialogueConditionManager.SharedInstance.SetConditionState("RH_SLATE_ATE_BAD_MALLOW", true);
+                    slatefire_badmallow_lastate = TimeLoop.GetSecondsElapsed();
+                }
+                else if (eskerfire_roasting)
+                {
+                    DialogueConditionManager.SharedInstance.SetConditionState("RH_ESKER_ATE_BAD_MALLOW", true);
+                    eskerfire_badmallow_lastate = TimeLoop.GetSecondsElapsed();
+                }
+                else if (riebeckfire_roasting)
+                {
+                    DialogueConditionManager.SharedInstance.SetConditionState("RH_RIEBECK_ATE_BAD_MALLOW", true);
+                    riebeckfire_badmallow_lastate = TimeLoop.GetSecondsElapsed();
+                }
+                else if (chertfire_roasting)
+                {
+                    DialogueConditionManager.SharedInstance.SetConditionState("RH_CHERT_ATE_BAD_MALLOW", true);
+                    chertfire_badmallow_lastate = TimeLoop.GetSecondsElapsed();
+                }
+                else if (feldsparfire_roasting)
+                {
+                    DialogueConditionManager.SharedInstance.SetConditionState("RH_FELDSPAR_ATE_BAD_MALLOW", true);
+                    feldsparfire_badmallow_lastate = TimeLoop.GetSecondsElapsed();
+                }
+            }
+        }
+
         // Dialogue variables
         public void OnEnterConversation()
         {
@@ -378,6 +490,28 @@ namespace ReactiveHearthians
             else
             {
                 DialogueConditionManager.SharedInstance.SetConditionState("RH_FELDSPAR_FIRE_DAMAGED_RECENT", false);
+            }
+
+            // Resetting badmallow dialogue, we do this differently than above but it's effectively the same thing //
+            if (DialogueConditionManager.SharedInstance.GetConditionState("RH_SLATE_ATE_BAD_MALLOW_DIALOGUE_THIS_LOOP") || TimeLoop.GetSecondsElapsed() - slatefire_badmallow_lastate >= 10)
+            {
+                DialogueConditionManager.SharedInstance.SetConditionState("RH_SLATE_ATE_BAD_MALLOW", false);
+            }
+            if (DialogueConditionManager.SharedInstance.GetConditionState("RH_RIEBECK_ATE_BAD_MALLOW_DIALOGUE_THIS_LOOP") || TimeLoop.GetSecondsElapsed() - riebeckfire_badmallow_lastate >= 10)
+            {
+                DialogueConditionManager.SharedInstance.SetConditionState("RH_RIEBECK_ATE_BAD_MALLOW", false);
+            }
+            if (DialogueConditionManager.SharedInstance.GetConditionState("RH_ESKER_ATE_BAD_MALLOW_DIALOGUE_THIS_LOOP") || TimeLoop.GetSecondsElapsed() - eskerfire_badmallow_lastate >= 10)
+            {
+                DialogueConditionManager.SharedInstance.SetConditionState("RH_ESKER_ATE_BAD_MALLOW", false);
+            }
+            if (DialogueConditionManager.SharedInstance.GetConditionState("RH_CHERT_ATE_BAD_MALLOW_DIALOGUE_THIS_LOOP") || TimeLoop.GetSecondsElapsed() - chertfire_badmallow_lastate >= 10)
+            {
+                DialogueConditionManager.SharedInstance.SetConditionState("RH_CHERT_ATE_BAD_MALLOW", false);
+            }
+            if (DialogueConditionManager.SharedInstance.GetConditionState("RH_FELDSPAR_ATE_BAD_MALLOW_DIALOGUE_THIS_LOOP") || TimeLoop.GetSecondsElapsed() - feldsparfire_badmallow_lastate >= 10)
+            {
+                DialogueConditionManager.SharedInstance.SetConditionState("RH_FELDSPAR_ATE_BAD_MALLOW", false);
             }
 
             // Misc variables //
